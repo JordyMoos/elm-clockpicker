2fa935c6cf7b94585232544dc92b22f32b97281c
Jordy Moos <jordymoos@gmail.com>
2016-11-16 08:33:50 +0100

    Start of the clockpicker

--- Main.elm
+++ Main.elm
@@ -2,6 +2,7 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Events exposing (..)
+import Html.Attributes exposing (..)
 import Task
 
 
@@ -17,17 +18,20 @@ main =
 
 type alias Model =
   { value : String
+  , isVisible : Bool
   }
 
 
 init : (Model, Cmd Msg)
 init =
-  (Model "", Cmd.none)
+  (Model "" False, Cmd.none)
 
 
 type Msg
   = NoOp
   | UpdateValue String
+  | OpenPicker
+  | ClosePicker
 
 
 update : Msg -> Model -> (Model, Cmd Msg)
@@ -36,14 +40,44 @@ update msg model =
     NoOp ->
       (model, Cmd.none)
 
+    OpenPicker ->
+      ({model | isVisible = True}, Cmd.none)
+
+    ClosePicker ->
+      ({model | isVisible = False}, Cmd.none)
+
     UpdateValue value ->
       ({model | value = value}, Cmd.none)
 
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ p [] [ text "Clockpicker" ]
-    , input [ onInput UpdateValue ] []
-    , text model.value
+  div
+    [ style [ ("background-color", "green") ]
+    , onBlur ClosePicker
     ]
+    [ p [] [ text "Clockpicker" ]
+    , input
+      [ onInput UpdateValue
+      , onClick OpenPicker
+      ]
+      []
+    , text model.value
+    , clockPickerWrapper model
+    ]
+
+
+clockPickerWrapper : Model -> Html Msg
+clockPickerWrapper model =
+  if model.isVisible then
+    drawClockPicker model
+  else
+    text ""
+
+
+drawClockPicker : Model -> Html Msg
+drawClockPicker model =
+  div []
+    [ p [] [ text "Fancy clockpicker" ]
+    ]
+
